e9146248db5580a48593fe1c2776cc4a49d82c95
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-06-18 09:24:07 +0430

    Add button | Save data in an array of objects | show alert (success or error)

---
+++ app.js
@@ -0,0 +1,46 @@
+const taskInput = document.getElementById("task-input") ;
+const dateInput = document.getElementById("date-input") ; 
+const addButton = document.getElementById("add-button") ; 
+const alertMessage = document.getElementById("alert-message") ; 
+
+const todos = []
+
+
+const showAlert  = (message , type) => { 
+    alertMessage.innerHTML  = "" ; 
+    const alert = document.createElement("p") ; 
+    alert.innerText = message ; 
+    alert.classList.add("alert") ; 
+    alert.classList.add(`alert-${type}`) ;
+
+    alertMessage.append(alert) ; 
+
+    setTimeout(()=> { 
+        alert.style.display = "none" ; 
+    } , 2500)
+
+}
+
+const addHandler = ()=> {
+    const task = taskInput.value.toLowerCase().trim(); 
+    const date = dateInput.value.toLowerCase().trim(); 
+    const todo = { 
+        'task' : task ,
+        'date' : date ,
+        'completed' : false
+    };
+    if (task==="") {
+
+        showAlert("Please enter a todo!" , "error") ; 
+    }
+    else { 
+        todos.push(todo) ;
+        taskInput.value = "" ;
+        dateInput.value = "" ; 
+        console.log(todos) ;
+
+        showAlert("Todo added successfully!" , "success") ;  
+    }
+}; 
+
+addButton.addEventListener("click" , addHandler) ; 
