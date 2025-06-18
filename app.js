673cbf1a25d55443ca1c0d95b133a1e1d19f3525
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-06-18 12:03:27 +0430

    Display all todos in the table

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const taskInput = document.getElementById("task-input") ;
 const dateInput = document.getElementById("date-input") ; 
 const addButton = document.getElementById("add-button") ; 
 const alertMessage = document.getElementById("alert-message") ; 
+const todosBody = document.querySelector("tbody") ; 
+
 let ID = 0 ; 
 
 let todos = JSON.parse(localStorage.getItem("todos")) ; 
@@ -10,7 +12,6 @@ if(todos === null) {
     todos = [] ; 
 }
 
-console.log(todos) ;
 
 
 const showAlert  = (message , type) => { 
@@ -32,6 +33,36 @@ const saveToLocalStorage = ()=> {
     localStorage.setItem("todos" , JSON.stringify(todos)) ; 
 }
 
+const displayTodos = () => { 
+    todosBody.innerHTML = "" ; 
+
+    if(!todos.length) { 
+        todosBody.innerHTML = "<tr>\
+        <td colspan = '4'>\
+        No task found.\
+        </td>\
+        </tr>";
+        
+        return;
+    }
+    else { 
+        todos.forEach(todo => {
+            todosBody.innerHTML += `
+            <tr>
+                <td>${todo.task}</td> 
+                <td>${todo.date ? todo.date : "No date."}</td> 
+                <td>${todo.completed ? "Completed" : "Pending"} </td> 
+                <td>
+                    <button>Edit</button>
+                    <button>Do</button>
+                    <button>Delete</button>
+                </td>     
+            `          
+        });
+    }
+}
+displayTodos() ;
+
 const addHandler = ()=> {
     ID = ID + 1 ; 
     const task = taskInput.value.toLowerCase().trim(); 
@@ -49,10 +80,10 @@ const addHandler = ()=> {
     else { 
         todos.push(todo) ;
         saveToLocalStorage();
+        displayTodos() ; 
         taskInput.value = "" ;
         dateInput.value = "" ; 
-        console.log(todos) ;
-        
+    
         showAlert("Todo added successfully!" , "success") ;  
     }
 }; 
